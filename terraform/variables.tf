465066b84ad4dc75ae5e1a6053f94818af82dbd7
flavlmne <flavlmne>
2026-07-28 11:46:45 +0200

    refactor(terraform): clean up variables.tf definitions and descriptions

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,36 +1,41 @@
-
 variable "aws_default_region" {
+  type        = string
   default     = "us-east-1"
-  description = "My AWS region"
+  description = "Région AWS pour le déploiement"
 }
 
 variable "default_ubuntu_ami" {
-  default     = "ami-0446f93cefa2981e5"
   type        = string
-  description = "Default Ubuntu AMI"
+  default     = "ami-0446f93cefa2981e5"
+  description = "AMI Ubuntu 22.04 LTS"
 }
 
 variable "default_vpc_id" {
-  default = "vpc-0d52b82865c0ed086"
-  type    = string
+  type        = string
+  default     = "vpc-0d52b82865c0ed086"
+  description = "ID du VPC AWS par défaut"
 }
 
 variable "default_public_subnet_id" {
-  default = "subnet-080466c956ef16d82"
-  type    = string
+  type        = string
+  default     = "subnet-080466c956ef16d82"
+  description = "ID du sous-réseau public"
 }
 
 variable "default_instance_type" {
-  default = "t2.micro"
+  type        = string
+  default     = "t2.micro"
+  description = "Type d'instance EC2"
 }
 
 variable "default_security_group_id" {
-  default = "sg-07f0ad52f78074f02"
-  type    = string
+  type        = string
+  default     = "sg-07f0ad52f78074f02"
+  description = "ID du groupe de sécurité AWS"
 }
 
 variable "key_name" {
-  default     = "vockey"
   type        = string
-  description = "Pre-existing AWS Key Pair name (vockey for Learner Lab)"
+  default     = "vockey"
+  description = "Nom de la paire de clés SSH AWS (vockey pour Learner Lab)"
 }
